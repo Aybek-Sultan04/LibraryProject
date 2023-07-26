d8a573b745700a6630d960e096ed179d86de804b
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-07-26 12:55:39 +0300

    Implemented new method GetUsers

---
+++ LibraryProject/DDManager/GetAllUsers.cs
@@ -0,0 +1,32 @@
+﻿using LibraryProject.Users;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace LibraryProject.DDManager
+{
+    internal class GetAllUsers
+    {
+        public static List<UserModel> GetUser()
+        {
+            using (StreamReader reader = new StreamReader("User.json", true))
+            {
+                try
+                {
+                    var users = JsonConvert.DeserializeObject<List<UserModel>>(reader.ReadToEnd());
+                    return users;
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return null;
+        }
+    }
+}
