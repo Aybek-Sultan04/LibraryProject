d521df3b70df8c04f5afdc9c32ab8a04ab278fd1
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-07-26 12:59:15 +0300

    create new Class AddNewUser and inlement new async method AddUserAsunc

---
+++ LibraryProject/Users/AddNewUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Users
+{
+    internal class AddNewUser
+    {
+        public static async Task AddUserAsync()
+        {
+            try
+            {
+                var user = new UserModel();
+                Console.Write("Enter name:");
+                user.UserName = Console.ReadLine();
+                Console.Write("Create your Password:");
+                user.UserPassword = Console.ReadLine();
+                Console.Write("Enter Age:");
+                user.Age= Convert.ToByte(Console.ReadLine());
+                user.DateRegistration = DateOnly.Parse(DateTime.Now.ToString());
+            wrongEmail:
+                Console.Write("Enter Email:");
+                string? currentEmail = Console.ReadLine();
+                string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+                if (Regex.IsMatch(currentEmail, pattern, RegexOptions.IgnoreCase))
+                    user.UserEmail = currentEmail;
+                else
+                {
+                    Console.WriteLine("Wrong Email!!!\nTry again.. ");
+                    goto wrongEmail;
+                }
+            }
+        }
+    }
+}
