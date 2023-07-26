95842edccaf75340d3dc7408cf32d5b94824b826
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-07-26 12:59:41 +0300

    add class and method check user

---
+++ LibraryProject/Users/CheckUser.cs
@@ -0,0 +1,33 @@
+﻿using LibraryProject.DDManager;
+using LibraryProject.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Users
+{
+    internal class CheckUser
+    {
+        public static void Check()
+        {
+            Console.Write("Enter Login:");
+            string? userLoging= Console.ReadLine();
+            Console.Write("Enter Password:");
+            string? userPassword = Console.ReadLine();
+            var allUSers = (from user in GetAllUsers.GetUser()
+                           where user.UserName.Equals(userLoging)
+                           where user.UserPassword.Equals(userPassword)
+                           select user).ToList();
+            if(allUSers!=null)
+            MainMenu.Menu();
+            else
+            {
+                Console.WriteLine("This user not found!\nTry again..");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
